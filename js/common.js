145f8d8ad6e9a7d8e450ed16848f58e8d1b7ecf2
slowjam83 <52689494+slowjam83@users.noreply.github.com>
2023-05-26 17:50:04 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,56 +1,87 @@
 $(document).ready(function () {
-    // loading script
-    $.fakeLoader({
-        timeToHide: 700,
-        bgColor: '#2d2d2f',
-        spinner: "spinner5"
-    });
-    
-    // mobile nav
-    $('.ham').click(function () {
-        $(this).toggleClass("active");
-        $('.mo-nav').toggleClass("active");
-    });
+  // social btn
+  $(".social_btn").mouseover(function () {
+    $(".util .social_group").addClass("active");
+  });
+  $(".util .social_group").mouseover(function () {
+    $(this).addClass("active");
+  });
+  $(".util .social_group").mouseout(function () {
+    $(this).removeClass("active");
+  });
 
-    $('.carousel').carousel({
-        interval: 5000
-    });
+  // language btn
+  $(".language_btn").mouseover(function () {
+    $(".util .language_group").addClass("active");
+  });
+  $(".util .language_group").mouseover(function () {
+    $(this).addClass("active");
+  });
+  $(".util .language_group").mouseout(function () {
+    $(this).removeClass("active");
+  });
 
-    // go top button
+  $(".language_group a").hover(function () {
+    if ($(".language_group a").hasClass("active")) {
+      $(".language_group a").removeClass("active");
+    } else {
+      $(this).addClass("active");
+    }
+  });
+
+  // loading script
+  $.fakeLoader({
+    timeToHide: 700,
+    bgColor: "#2d2d2f",
+    spinner: "spinner5",
+  });
+
+  // mobile nav
+  $(".ham").click(function () {
+    $(this).toggleClass("active");
+    $(".mo-nav").toggleClass("active");
+  });
+
+  // $('.carousel').carousel({
+  //     interval: 5000
+  // });
+
+  // go top button
+  btnTop();
+
+  $(document).on("scroll", function () {
     btnTop();
+  });
 
-    $(document).on('scroll', function () {
-        btnTop();
+  $(document).on("click", "#goTop", function () {
+    $("html,body").animate({
+      scrollTop: "0",
     });
+    return false;
+  });
 
-    $(document).on("click", "#goTop", function () {
-        $("html,body").animate({
-            "scrollTop": "0"
-        });
-        return false;
-    });
+  function btnTop() {
+    var scrollTop = $(window).scrollTop();
 
-    function btnTop() {
-        var scrollTop = $(window).scrollTop();
-        
-        if (scrollTop > 0) {
-            $("#goTop").stop().show().animate({
-                "opacity": "1"
-            }, 300);
-            
-            $(".write_btn").stop().animate({
-                "bottom": "144px"
-            }, 300);
-        } else {
-            $("#goTop").stop().animate({
-                "opacity": "0"
-            }, 300, function () {
-                $("#goTop").hide();
-            });
-            
-            $(".write_btn").stop().animate({
-                "bottom": "74px"
-            }, 300);
-        }
-    };    
+    if (scrollTop > 0) {
+      $("#goTop").stop().show().animate(
+        {
+          opacity: "1",
+        },
+        300
+      );
+    } else {
+      $("#goTop")
+        .stop()
+        .animate(
+          {
+            opacity: "0",
+          },
+          300,
+          function () {
+            $("#goTop").hide();
+          }
+        );
+    }
+  }
 });
